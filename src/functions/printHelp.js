bc4de4a1b5bdc7f7257b078573c88df861336c80
Pankaj Patel <pankaj.patel@hotjar.com>
2022-01-02 22:52:50 +0100

    1st test & road to better code arrangement

---
+++ src/functions/printHelp.js
@@ -0,0 +1,15 @@
+const pkg = require('../../package');
+const { optionsText } = require('../constants');
+
+function printHelp(linePrinter = console.log) {
+  const lines = [
+    `${pkg.name} ${pkg.version}`,
+    pkg.description,
+    'Usage:',
+    `${pkg.name} [path] [options]`,
+    optionsText,
+  ];
+  linePrinter(lines.join('\n'));
+}
+
+module.exports = printHelp;
