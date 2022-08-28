8ef8c155411282c82b94bf53abc1fddaad1ac2f8
Pankaj Patel <patel.pankaj@live.in>
2022-08-28 18:23:02 +0200

    breakdown codeclimate function (#169)

--- src/functions/getCommandOptions.ts
+++ src/functions/getCommandOptions.ts
@@ -2,39 +2,39 @@ import minimist from 'minimist';
 import { getCompactness } from './getCompactness';
 import constants, { SORT_DIRECTIONS } from '../constants';
 
+const extractOptions = (argv: minimist.ParsedArgs) => ({
+  // Output Flags & Options
+  showGitOnly: argv.gitonly || argv.g,
+  needsAttention: argv.attention || argv.a,
+  shouldShowSimpleOutput: argv.simple || argv.s,
+
+  // Help Flags
+  shouldShowHelp: argv.help || argv.h,
+  shouldShowVersion: argv.version || argv.v,
+});
+
 export const getCommandOptions = (
   _argv: NodeJS.Process['argv'],
   cwd: string
 ): CommandOptions => {
   const argv = minimist(_argv.slice(2));
 
-  const showGitOnly = argv.gitonly || argv.g;
-  const shouldShowHelp = argv.help || argv.h;
-  const needsAttention = argv.attention || argv.a;
-  const shouldShowVersion = argv.version || argv.v;
-  const shouldShowSimpleOutput = argv.simple || argv.s;
   const dir = argv._.length ? argv._[0] : cwd;
   const shouldSort = Boolean(argv.sort);
   const sortDirection: string =
     shouldSort && typeof argv.sort === 'string'
       ? String(argv.sort).toUpperCase()
       : SORT_DIRECTIONS.ASC;
-  const compactness = getCompactness(argv);
   const debug = Boolean(argv.debug) ? console.log : constants.NOOP;
 
   return {
+    ...extractOptions(argv),
     cwd: dir,
     debug,
     // Sorting options
     shouldSort,
     sortDirection,
     // Output Flags & Options
-    compactness,
-    showGitOnly,
-    needsAttention,
-    shouldShowSimpleOutput,
-    // Help Flags
-    shouldShowHelp,
-    shouldShowVersion,
+    compactness: getCompactness(argv),
   };
 };
